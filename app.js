017313e51b84966d08235a8ecbb0a9bd87721d20
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-17 17:45:46 -0300

    Refator app.js

--- app.js
+++ app.js
@@ -1,62 +1,71 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    routes = require('./routes/index'),
+    app = express();
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'public/views'));
-app.set('view engine', 'jade');
+setupViews();
+setupMiddlewares();
+setupErrorHandlers();
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public/assets')));
-app.use(express.static(path.join(__dirname, 'public/assets')));
+require('./config/database');
 
 app.use('/', routes);
-app.use('/users', users);
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+
+function setupViews() {
+  app.set('views', path.join(__dirname, 'public/views'));
+  app.set('view engine', 'jade');
+}
+
+function setupMiddlewares() {
+  app.use(favicon());
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded());
+  app.use(cookieParser());
+  app.use(require('less-middleware')(path.join(__dirname, 'public/assets')));
+  app.use(express.static(path.join(__dirname, 'public/assets')));
+
+  /// catch 404 and forward to error handler
+  app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+  });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+function setupErrorHandlers() {
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+      app.use(function(err, req, res, next) {
+          res.status(err.status || 500);
+          res.render('error', {
+              message: err.message,
+              error: err
+          });
+      });
+  }
 
-
-module.exports = app;
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: {}
+      });
+  });
+}
