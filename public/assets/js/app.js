2acde2c880bd8e3fccb1e5931a68024b387886ea
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-18 21:01:25 -0300

    Refactor; Change environment directive is working now

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,18 @@
-angular.module('app', []);
+angular.module('app', [])
+
+.directive('active', function ($rootScope) {
+  return {
+    link: function (scope, el, attrs) {
+      if($rootScope.activeEnv === attrs.active) {
+        el.addClass('active').siblings().removeClass('active');
+      }
+
+      el.click(function() {
+        el.addClass('active').siblings().removeClass('active');
+        $rootScope.$apply(function () {
+          $rootScope.activeEnv = attrs.active;
+        });
+      });
+    }
+  }
+});
