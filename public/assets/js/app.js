77342f5e3a3af69e59bdd6757b2159a775060ae2
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-25 19:13:04 -0300

    Updates the time on the UI

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,20 +1,30 @@
 angular.module('app', [])
 
-.directive('active', function ($rootScope) {
+.directive('activeEnv', function ($rootScope) {
   return {
     link: function (scope, el, attrs) {
-      if($rootScope.activeEnv === attrs.active) {
+      if($rootScope.activeEnv === attrs.activeEnv) {
         el.addClass('active').siblings().removeClass('active');
       }
 
       el.click(function() {
         el.addClass('active').siblings().removeClass('active');
         $rootScope.$apply(function () {
-          $rootScope.activeEnv = attrs.active;
+          $rootScope.activeEnv = attrs.activeEnv;
         });
       });
     }
   }
+})
+
+.directive('timeAgo', function () {
+  return {
+    link: function (scope, el, attrs) {
+      setInterval(function () {
+        el.text(moment(attrs.timeAgo).fromNow());
+      }, 60 * 1000);
+    }
+  };
 })
 
 .filter('timeAgo', function () {
