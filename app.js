1fe0e4742062c797b3489151700f9fc6ca38201d
Rafael Garcia <rafbgarcia@gmail.com>
2014-05-25 16:23:31 -0300

    Refactors app.js

--- app.js
+++ app.js
@@ -12,52 +12,36 @@ var express      = require('express'),
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-app.set('views', path.join(__dirname, 'public/views'));
-app.set('view engine', 'jade');
+setupViews();
+setupMiddlewares();
+setupErrorHandlers();
+setupDatabase();
+setupRoutes();
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public/assets')));
-app.use(express.static(path.join(__dirname, 'public/assets')));
+startServer();
 
-require('./config/database');
-
-app.use('/', routes);
-
-var serverInstance;
-
-global.io = io;
-
-exports.startServer = function(cb) {
+function startServer () {
   app.set('port', process.env.PORT || 3000);
 
-  serverInstance = server.listen(app.get('port'), function () {
-    typeof cb === 'function' && cb();
-  });
-};
-
-exports.closeServer = function(cb) {
-  if (serverInstance) {
-    serverInstance.close(cb);
-  }
-}
-
-// when app.js is launched directly
-if (module.id === require.main.id) {
-  exports.startServer(function() {
+  server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
   });
 }
 
-function setupViews(app) {
+function setupRoutes () {
+  app.use('/', routes);
+}
+
+function setupDatabase () {
+  require('./config/database');
+}
+
+function setupViews () {
   app.set('views', path.join(__dirname, 'public/views'));
   app.set('view engine', 'jade');
 }
 
-function setupMiddlewares(app) {
+function setupMiddlewares () {
   app.use(favicon());
   app.use(logger('dev'));
   app.use(bodyParser.json());
@@ -65,16 +49,9 @@ function setupMiddlewares(app) {
   app.use(cookieParser());
   app.use(require('less-middleware')(path.join(__dirname, 'public/assets')));
   app.use(express.static(path.join(__dirname, 'public/assets')));
-
-  /// catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
 }
 
-function setupErrorHandlers(app) {
+function setupErrorHandlers () {
   // development error handler
   // will print stacktrace
   if (app.get('env') === 'development') {
