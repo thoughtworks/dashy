4faa33c4658cd3eb87c3f79ebd61a69839d8fa90
Alabê + Álvaro <aduarte@thoughtworks.com>
2014-05-28 17:58:40 -0300

    +Álvaro - Setting up testing environment to work with karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,22 @@ module.exports = function (grunt) {
       watch_test: {
         command: './node_modules/jasmine-node/bin/jasmine-node spec/ --autotest --watch spec/ models/ config/ routes/ --color --captureExceptions'
       }
+    },
+    karma: {
+      unit: {
+        options: {
+          singleRun: true,
+          frameworks: ['jasmine'],
+          files: ['public/assets/js/test/**/*.js'],
+          browsers: ['PhantomJS']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('test', ['exec:test']);
+  grunt.registerTask('test', ['exec:test', "karma:unit"]);
   grunt.registerTask('watch_test', ['exec:watch_test']);
 };
